ccf8aef4e223694810ca9cbd17a7711f587c785f
Dou Tianqi <159125210+QuickFcodes@users.noreply.github.com>
2024-06-02 10:31:53 +0800

    Update vm.c

--- vm.c
+++ vm.c
@@ -1,11 +1,11 @@
 #include "vm.h"
 
-typedef int vm[1024];
+typedef int vm[RAMMAX];
 
 vm mbook={zerod};
 
 int erx[64]={zeros},i=0,iflags=0;
-int stack[1024]={zerod};
+int stack[RAMMAX]={zerod};
 int pos=0;
 void error(int codes) {
     switch (codes)
@@ -29,7 +29,7 @@ void error(int codes) {
 }
 void push(int num) {
     stack[pos] = num;
-    pos<1024?pos++:0;
+    pos<RAMMAX?pos++:0;
 }
 int pop(void) {
     return pos<0?pos++:0,stack[pos--];
@@ -50,7 +50,7 @@ void ints(int codes) {
     }
 }
 void eval() {
-    for(;i<1024;i++) {
+    for(;i<RAMMAX;i++) {
         switch (mbook[i]) {
         case RET:
             i=pop();
@@ -79,12 +79,12 @@ void eval() {
             break;
         case MOVC://mov reg ram
             if(mbook[i+1]>=64)error(0);
-            if(mbook[erx[mbook[i+2]]]>=1024)error(1);
+            if(mbook[erx[mbook[i+2]]]>=RAMMAX)error(1);
             erx[mbook[i+1]] = mbook[erx[mbook[i+2]]];
             i+=2;
             break;
         case MOVRC:// mov ram reg
-            if(mbook[i+1]>=1024)error(1);
+            if(mbook[i+1]>=RAMMAX)error(1);
             if(mbook[i+2]>=64)error(0);
             mbook[mbook[i+1]] = erx[mbook[i+2]];
             i+=2;
@@ -100,23 +100,23 @@ void eval() {
             i++;
             break;
         case JMP:
-            if(mbook[i+1]>=1024)error(1);
+            if(mbook[i+1]>=RAMMAX)error(1);
             i=mbook[i+1];
             i--;
             break;
         case JMR:
-            if(erx[mbook[i+2]]>=1024)error(0);
+            if(erx[mbook[i+2]]>=RAMMAX)error(0);
             i=erx[mbook[i+1]];
             i--;
             break;
         case CALL:
-            if(mbook[i+1]>=1024)error(1);
+            if(mbook[i+1]>=RAMMAX)error(1);
             push(i+2);
             i=mbook[i+1];
             i--;
             break;
         case CALLC:
-            if(erx[mbook[i+1]]>=1024)error(1);
+            if(erx[mbook[i+1]]>=RAMMAX)error(1);
             push(i+2);
             i=erx[mbook[i+1]];
             i--;
@@ -177,32 +177,32 @@ void eval() {
             i+=2;
             break;
         case JAE:
-            if(mbook[i+1]>=1024)error(1);
+            if(mbook[i+1]>=RAMMAX)error(1);
             if(iflags==0)i=mbook[i+1];
             i--;
             break;
         case JQE:
-            if(mbook[i+1]>=1024)error(1);
+            if(mbook[i+1]>=RAMMAX)error(1);
             if(iflags==1)i=mbook[i+1];
             i--;
             break;
         case JKE:
-            if(mbook[i+1]>=1024)error(1);
+            if(mbook[i+1]>=RAMMAX)error(1);
             if(iflags==2)i=mbook[i+1];
             i--;
             break;
         case JNE:
-            if(mbook[i+1]>=1024)error(1);
+            if(mbook[i+1]>=RAMMAX)error(1);
             if(iflags!=0)i=mbook[i+1];
             i--;
             break;
         case JCE:
-            if(mbook[i+1]>=1024)error(1);
+            if(mbook[i+1]>=RAMMAX)error(1);
             if((iflags==1) || (iflags==0))i=mbook[i+1];
             i--;
             break;
         case JIE:
-            if(mbook[i+1]>=1024)error(1);
+            if(mbook[i+1]>=RAMMAX)error(1);
             if((iflags==2) || (iflags==0))i=mbook[i+1];
             i--;
             break;
@@ -224,4 +224,3 @@ int main(int argc,char ** argv) {
     use(2) = EXITC;
     eval();
 }
-
